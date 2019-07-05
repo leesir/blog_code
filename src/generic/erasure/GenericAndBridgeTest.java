8506aa1f8e1e55341db5ec847484b4c460874546
李璟 <lijing04@kaisagroup.com>
2019-07-05 09:22:09 +0800

    新增博客代码

--- src/generic/erasure/GenericAndBridgeTest.java
+++ src/generic/erasure/GenericAndBridgeTest.java
@@ -1,4 +1,31 @@
 package generic.erasure;
 
 public class GenericAndBridgeTest {
+
+    public class Node<T> {
+
+        public T data;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public void setData(T data) {
+            System.out.println("Node.setData");
+            this.data = data;
+        }
+    }
+
+    public class MyNode extends Node<Integer> {
+        public MyNode(Integer data) {
+            super(data);
+        }
+
+        @Override
+        public void setData(Integer data) {
+            System.out.println("MyNode.setData");
+            super.setData(data);
+        }
+
+    }
 }
--- src/generic/erasure/GenericClassErasureTest.java
+++ src/generic/erasure/GenericClassErasureTest.java
@@ -1,5 +1,7 @@
 package generic.erasure;
 
+import java.util.List;
+
 /**
  * 泛型类擦除测试代码
  *
@@ -10,6 +12,12 @@ public class GenericClassErasureTest {
     public static void main(String[] args) {
         Node<String> node = new Node<>("tail", null);
         System.out.println(node.getData());
+        UpperBoundTypeClass<String> boundTypeClass = new UpperBoundTypeClass<>("NodeSubClass");
+        System.out.println(boundTypeClass.getData());
+    }
+
+    public static <T> void f(List<? super Node> n) {
+
     }
 
     public static class Node<T> {
@@ -21,6 +29,20 @@ public class GenericClassErasureTest {
             this.next = next;
         }
 
-        public T getData() { return data; }
+        public T getData() {
+            return data;
+        }
+    }
+
+    public static class UpperBoundTypeClass<T extends String> {
+        private T data;
+
+        public UpperBoundTypeClass(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
     }
 }
