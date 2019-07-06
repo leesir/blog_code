b400b660f4f91eb53c2cd519847605acec2fba78
李璟 <lijing@lijings-MBP.lan>
2019-07-07 01:15:29 +0800

    modify blog code

--- src/generic/boundandWildcard/GenericBoundTest.java
+++ src/generic/boundandWildcard/GenericBoundTest.java
@@ -1,5 +1,6 @@
 package generic.boundandWildcard;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,7 +18,11 @@ public class GenericBoundTest {
         iter(new HashSet<>(Arrays.asList("1", "2", "a")));
 
         //compile fail
-        computeNumber("string1", number2);
+//        computeNumber("string1", number2);
+
+        computeCloneableNumber(new CloneableInteger(1));
+        visitCAndSObject(new CAndS(){});
+
     }
 
     public static <T extends Number> void computeNumber(T number1, T number2) {
@@ -26,8 +31,54 @@ public class GenericBoundTest {
 
     public static <T extends Collection> void iter(T collection) {
         //iterate
-        for (Object ele : collection) {
-            System.out.println(ele);
+        if (!collection.isEmpty()) {
+            for (Object ele : collection) {
+                System.out.println(ele);
+            }
+        }
+    }
+
+    public static <T extends Number & Cloneable> void computeCloneableNumber(T number) {
+        System.out.println(number.intValue());
+    }
+
+    public static <T extends Serializable & Cloneable> void visitCAndSObject(T object) {
+        System.out.println(object);
+    }
+
+    public static interface CAndS extends Cloneable, Serializable {
+
+    }
+
+    public static class CloneableInteger extends Number implements Cloneable {
+        private Integer data;
+
+        public CloneableInteger(Integer data) {
+            this.data = data;
+        }
+
+        public Integer getData() {
+            return data;
+        }
+
+        @Override
+        public int intValue() {
+            return data;
+        }
+
+        @Override
+        public long longValue() {
+            return data;
+        }
+
+        @Override
+        public float floatValue() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public double doubleValue() {
+            throw new UnsupportedOperationException();
         }
     }
 }
